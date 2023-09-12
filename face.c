1800c3c005ff43ad157a7d1d4296b020a1841fee
root <root@PC>
2023-09-12 16:06:14 +1000

    uploading tut 1 code, make sure to compi le yourself before using

---
+++ face.c
@@ -0,0 +1,30 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+#include <stdio.h>
+
+/*
+the main function
+
+
+more comments
+
+this is poor style
+*/
+int main (void) {
+    printf("Hello World\nHello again\n");
+
+    printf("~ ~");
+    printf("0 0");
+    printf(" o");
+    printf(" -");
+    print("\n")
+
+    printf("~ ~");
+    printf("0 0");
+    printf(" o");
+    printf("\\_/");
+    print("\n")
+    
+    return 0;
+}
