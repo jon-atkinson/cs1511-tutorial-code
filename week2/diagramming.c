f0e10e28beadeae33de2b02867d8a51dcaa4ee9c
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2023-09-19 00:39:02 +1000

    updated error procedure and commenting for week 2 (specifically diagramming)

--- week2/diagramming.c
+++ week2/diagramming.c
@@ -3,22 +3,30 @@
 
 #include <stdio.h>
 
-void print_error_1();
+// procedure prototypes
+void print_error_1(int a, int b);
 void print_div_by_zero_error();
 
 int main(void) {
     printf("please enter a and b: ");
+    // not the reccomended format for declaring variables but here so that you can see it
+    // being used
     int a, b;
     scanf("%d %d", &a, &b);
 
     if (a < b) {
-        print_error_1();
+        // note the use of a procedure here to print the error message, which is requested
+        // by the specification
+        print_error_1(a, b);
+        return 1;
     }
 
     if (b == 0) {
         print_div_by_zero_error();
+        return 1;
     }
 
+    // note the .5 in the %.5f, this limits the %f to 5 decimal places
     if (a > b) {
         printf("a/b is %d and (a*0.1)/(b*0.1) is %.5f\n", a/b, (a*0.1)/(b*0.1));
     }
@@ -26,10 +34,10 @@ int main(void) {
     return 0;
 }
 
-void print_error_1() {
-        printf("error: a was less than b\n");
+void print_error_1(int a, int b) {
+    printf("error: %d is less than %d\n", a, b);
 }
 
 void print_div_by_zero_error() {
-        printf("b is zero, cannot divide by 0\n");
+    printf("b is zero, cannot divide by 0\n");
 }
