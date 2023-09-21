8c0a5d8b80bb8e6c6fa75740fb5e201e4d230960
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2023-09-22 00:24:26 +1000

    adding Smayan's soln. to repo

---
+++ week2/int_assignment.c
@@ -0,0 +1,26 @@
+// auth: Smayan G
+// date: 22/09/2023
+
+#include <stdio.h> 
+
+int main(void) {
+
+    int a;
+    int b;
+    printf ("Enter the first integer: ");
+    scanf("%d", &a);
+    printf("Enter the second integer: ");
+    scanf("%d", &b);
+
+    if (a<b) {
+        printf("error, please try again\n");
+    }
+    else if (b==0) {
+        printf("error, invalid input\n");
+    }
+    else { 
+        printf ("The output is %d, %lf\n", a/b, (a*0.1)/(b*0.1));
+    }
+    return 0;
+
+}
