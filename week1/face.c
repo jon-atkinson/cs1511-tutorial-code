3e892eb38228cd2fc52495571d5d56f857aecb1c
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-02-15 23:20:51 +1100

    adding weekk 1 code and slides to repo

---
+++ week1/face.c
@@ -0,0 +1,37 @@
+// face.c
+//
+// Written by COMP1511 H13B,
+// on 15/02/2024
+//
+// The program prints two simple faces
+//
+// Face 1:
+//  ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+
+#include <stdio.h>
+
+int main(void) {
+    // first face
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n\n");
+
+    // second face
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+
+    return 0;
+}
