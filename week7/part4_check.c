cb60c3dc20935c0fdae4e5b114eb8f916493fea4
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-05-25 19:25:13 +1000

    synch

--- week7/part4_check.c
+++ week7/part4_check.c
@@ -1,6 +1,6 @@
 // Check for Command Line Arguments
-// This program checks if any command-line arguments were provided except for 
-// the program name. If none were provided, print a message indicating so; 
+// This program checks if any command-line arguments were provided except for
+// the program name. If none were provided, print a message indicating so;
 // otherwise, print the number of arguments.
 // Written by COMP1511 T09B, on March 2024
 
@@ -11,6 +11,12 @@ int main(int argc, char *argv[]) {
         printf("No command-line arguments provided.\n");
     } else {
         printf("Number of arguments: %d\n", argc - 1);
+    }
+
+    int i = 0;
+    while (i < argc) {
+        printf("%d\n", i);
+        i++;
     }
 
     return 0;
