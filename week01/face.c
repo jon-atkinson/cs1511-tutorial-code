9a1db17b4b828d8a5896a092a3a36c3be5a80415
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-05-28 20:42:06 +1000

    adding week 1 slides and code to class repo

---
+++ week01/face.c
@@ -0,0 +1,24 @@
+// face.c
+//
+// Written by T09B 24T2,
+//
+// The program prints two simple faces note the double backslashes to print a single backslash
+
+#include <stdio.h>
+
+int main(void) {
+    
+    // Face 1
+    printf("  ~ ~\n");
+    printf("  0 0\n");
+    printf("   o\n");
+    printf("   -\n");
+    
+    // Face 2
+    printf("  ~ ~\n");
+    printf("  0 0\n");
+    printf("   o\n");
+    printf("  \\_/\n");
+
+    return 0;
+}
