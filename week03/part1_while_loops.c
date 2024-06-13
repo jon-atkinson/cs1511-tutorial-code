953de900c225fb4c18809438b7955be7a1fe7ffb
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-06-14 00:53:29 +1000

    adding week 3 tut code and slides

---
+++ week03/part1_while_loops.c
@@ -0,0 +1,20 @@
+// part1_while_loops.c
+//
+// Written by H15A
+// on 13/06/2024
+//
+// This program is a simple demonstration of a count loop in c
+
+
+#include <stdio.h>
+
+int main(void) {
+    int count = 0;
+
+    while (count < 10) {
+        printf("%d\n", count);
+        count++;
+    }
+
+    return 0;
+}
