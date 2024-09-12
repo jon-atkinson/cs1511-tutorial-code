38f9eeac5fe13ef8c7f6473db15b056440f94c0b
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-09-12 23:34:38 +1000

    adding slides and rand directories

---
+++ rand/random.c
@@ -0,0 +1,12 @@
+// random.c
+// Jon Atkinson, Sep 24
+
+#include <stdio.h>
+
+int main(void) {
+    printf("this is a random directory, it contains random things that don't \
+fit anywhere else in this repo (i.e. tuts/slides)\n");
+
+    return 0;
+}
+
