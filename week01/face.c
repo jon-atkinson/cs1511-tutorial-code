0f8121399e6efd05f66b52d5e1cb22a465adb2b0
Jon <95665780+jon-atkinson@users.noreply.github.com>
2024-09-12 22:25:35 +1000

    adding week 1 code

---
+++ week01/face.c
@@ -0,0 +1,25 @@
+// face.c
+//
+// Written by T09A,
+// on 10/09/24
+//
+// The program prints two simple faces
+//
+#include <stdio.h>
+
+int main (void) {
+    // Face 1:
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    // Face 2:
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    return 0;
+}
+
