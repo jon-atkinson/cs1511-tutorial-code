a6b29bdd9f731baa9e1f5d45801365928bd72387
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-09-13 01:36:33 +1000

    add tut code and slides for week 1, add rand directory

---
+++ week01/face.c
@@ -0,0 +1,25 @@
+// face.c
+//
+// Written by W09B,
+// on 11/09/24
+//
+// The program prints two simple faces
+//
+#include <stdio.h>
+
+int main (void) {
+    // Face 1:
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    // Face 2:
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    return 0;
+}
+
