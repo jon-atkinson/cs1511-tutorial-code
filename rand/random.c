98587aa5a4ba953b19bc8b5c73f7f791e42b65d8
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-09-14 17:42:33 +1000

    comment explaining multiline strings

--- rand/random.c
+++ rand/random.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 
 int main(void) {
+    // we don't usually recommend making your strings this long but if you do
+    // need to wrap them, this is the correct method
     printf("this is a random directory, it contains random things that don't \
 fit anywhere else in this repo (i.e. tuts/slides)\n");
 
