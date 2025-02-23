cc5b6506d12c213145dafd3015926b2695532416
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-02-23 16:30:32 +1100

    adding week01 starter code

---
+++ cs1511-25t1-t09b/week01/code/face.c
@@ -0,0 +1,36 @@
+// face.c
+//
+// Written by T09B,
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+#include <stdio.h>
+
+int main(void) {
+
+  // Face 1
+  printf("  ~ ~\n");
+  printf("  0 0\n");
+  printf("   o\n");
+  printf("   -\n");
+
+  // Face 2
+  printf("  ~ ~\n");
+  printf("  0 0\n");
+  printf("   o\n");
+  printf("  \\_/\n");
+
+  return 0;
+}
