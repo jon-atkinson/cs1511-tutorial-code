04b3847ba08f3b4bf8f68efdfd7a5b12ebde35fd
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-02-27 08:54:37 +1100

    adding week 2 code

---
+++ cs1511-25t1-h09b/week02/code/part1_variables.c
@@ -0,0 +1,24 @@
+// part1_variables
+//
+// This program was written by T09B
+//
+// This program calculates the area of a circle
+
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main(void) {
+  // Declaring the variable
+  int radius;
+  printf("Please enter the radius of your circle: ");
+
+  // Initalising the variable with user input
+  scanf("%d", &radius);
+
+  // Calculating the area
+  double area = (radius * radius) * PI;
+
+  printf("The area of the circle with a radius of %d is %lf\n", radius, area);
+  return 0;
+}
---
+++ cs1511-25t1-h09b/week02/code/part5_roller_coaster_improved.c
@@ -0,0 +1,56 @@
+// part5_roller_coaster
+//
+// This program was written by T09B
+//
+// This program:
+// 1. Scans in the users height.
+//
+// 2. If the height is 0 or less,
+//    it should print an error message
+//
+// 3. If the height is below the minimum height,
+//    it should print a message telling the user they are not tall enough to
+//    ride
+//
+// 4. If the height is above the minimum but below the ride alone threshold,
+//    it should print a message telling the user they can ride with an adult
+//
+// 5. If the height is or is above the ride alone threshold,
+//    it should print a message telling the user they can ride.
+
+#include <stdio.h>
+
+#define MINIMUM_HEIGHT 100
+#define RIDE_ALONE_HEIGHT 160
+
+int main(void) {
+  // 1. Scans in the users height.
+  printf("Please enter your height: ");
+  double height;
+  scanf("%lf", &height);
+
+  if (height <= 0) {
+    // 2. If the height is 0 or less,
+    //    it should print an error message
+    printf("Invalid height!\n");
+  } else if (height < MINIMUM_HEIGHT) {
+    // 3. If the height is below the minimum height,
+    //    it should print a message telling the user they are not tall enough to
+    //    ride
+    printf("Sorry, you are not tall enough to ride :<\n");
+  } else if (height < RIDE_ALONE_HEIGHT) {
+    // notice for this condition that we have already removed the search space
+    // of the lower bound and so can exclude the other half of the condition
+    // here
+
+    // 4. If the height is above the minimum but below the ride alone threshold,
+    //    it should print a message telling the user they can ride with an adult
+    printf("Yay! You are tall enough to ride. But only with an adult!\n");
+  } else {
+    // 5. If the height is or is above the ride alone threshold,
+    //    it should print a message telling the user they can ride.
+    printf("Congratulations! You are tall enough to ride alone!\n");
+  }
+
+  return 0;
+}
