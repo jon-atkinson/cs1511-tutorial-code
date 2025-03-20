6c565b269f1fc7e9175e1683b0125b168403f698
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-03-20 18:37:54 +1100

    adding simple example about leaving expression results unused

---
+++ cs1511-25t1-t09b/revision/assigning.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int num = 5;
+
+  printf("first: num is %d\n", num);
+
+  num = num + 1;
+  // num + 1;
+
+  printf("second: num is %d\n", num);
+
+  return 0;
+}
