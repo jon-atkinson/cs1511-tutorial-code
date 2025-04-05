cf122e3eaa7bcc4966511dbd8922eeca50a55c55
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-04-06 02:16:32 +1000

    adding week 7 tut code

---
+++ cs1511-25t1-t09b/revision/array_pointers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("3 factorial is %d\n", 3 * 2 * 1);
+  printf("7 factorial is %d\n", 7 * 6 * 5 * 4 * 3 * 2 * 1);
+
+  return 0;
+}
---
+++ cs1511-25t1-t09b/week07/part4_command_line_args.c
@@ -0,0 +1,21 @@
+// part4_command_line_args.c
+// T09B 25T1
+// a small program that prints all command line arguments passed to it,
+// including the file name
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    printf("argument %d is %s\n", i, argv[i]);
+  }
+
+  if (strcmp(argv[1], "first") == 0) {
+    printf("string was first\n");
+  } else {
+    printf("string was not first\n");
+  }
+
+  return 0;
+}
