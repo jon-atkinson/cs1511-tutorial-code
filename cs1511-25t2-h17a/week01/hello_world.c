a57a22e2e5a3000bf27c4015d52767beea6aed32
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-06-06 21:58:14 +1000

    adding week 1 tut code solutions for m15x and h17a

---
+++ cs1511-25t2-h17a/week01/hello_world.c
@@ -0,0 +1,13 @@
+// Basic Hello, World! progam
+
+#include <stdio.h>
+
+int main(void) {
+  // This prints "Hello, World!" to the terminal, and the next terminal
+  // commend is printed on the line below it, instead of the on the same
+  // line.
+  // What does this imply about what '\n' does?
+  printf("Hello, World!\n");
+
+  return 0;
+}
